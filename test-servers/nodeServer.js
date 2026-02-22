0e0aa1a90c6b334da7bac5a9d0bec2ce1cc8f689
Imranullah Khan <imranullahkhann2004@gmail.com>
2026-02-22 11:18:15 +0500

    Make the node server only serve / route

--- test-servers/nodeServer.js
+++ test-servers/nodeServer.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!\n');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found\n');
+  }
 });
 
 server.listen(8080, () => {
